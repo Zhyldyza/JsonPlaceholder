0a5a311917db5a4f07c9bfb4ad8f4cab22ddaf43
jyldyz <akylbekova@csdisco.com>
2023-04-04 14:27:06 +0600

    refactor

--- src/test/java/Assert.java
+++ src/test/java/Assert.java
@@ -15,25 +15,15 @@ public class Assert extends Testconfig {
 
     public String baseURI = "https://jsonplaceholder.typicode.com/";
     @Test
-     public void stringAssertionGetSingleElements() {
-      given().contentType(ContentType.JSON). //get response and check id is not null
-              when().get(baseURI + PLACEHOLDER_GET2).
-              then().statusCode(200).body(
-                      "id", notNullValue(),
-                      "userId",equalTo(1),
-                      "title",startsWith("sunt")).log().body();
-    }
-
-    @Test
     public void getAllElements(){
-                given().queryParam("postId", 1).log().uri().contentType(ContentType.JSON).//check 1 object
+                given().queryParam("postId", 1).contentType(ContentType.JSON).log().uri().
                         when().get(baseURI + PLACEHOLDER_GET4).
                         then().
                         body("[0].id", is(1),
                                 "[0].name" , containsString("id"),
                                    "[0].email", is("Eliseo@gardner.biz"));
 
-                given().queryParam("postId", 1).log().uri().contentType(ContentType.JSON).//check 2 object
+                given().queryParam("postId", 1).contentType(ContentType.JSON).log().uri().
                         when().get(baseURI + PLACEHOLDER_GET4).
                         then().
                         body("[1].id", notNullValue(),
@@ -41,7 +31,7 @@ public class Assert extends Testconfig {
                                 "[1].email",not("Eliseo@gardner.biz"),
                                 "[1].body",notNullValue());
 
-                given().queryParam("postId", 1).contentType(ContentType.JSON).//check 3 object
+                given().queryParam("postId", 1).contentType(ContentType.JSON).log().uri().
                         when().get(baseURI + PLACEHOLDER_GET4).
                         then().
                         body("[2].id", notNullValue(),
@@ -49,7 +39,7 @@ public class Assert extends Testconfig {
                                 "[2].email",notNullValue(),
                                 "[2].body",startsWith("q"));
 
-               given().queryParam("postId", 1).log().uri().contentType(ContentType.JSON).//check 4 object
+               given().queryParam("postId", 1).contentType(ContentType.JSON).log().uri().
                         when().get(baseURI + PLACEHOLDER_GET4).
                         then().
                         body("[3].id", greaterThan(2),
@@ -57,13 +47,13 @@ public class Assert extends Testconfig {
                                 "[3].email",endsWith("v"),
                                 "[3].body",startsWithIgnoringCase("Non"));
 
-                given().queryParam("postId", 1).log().uri().contentType(ContentType.JSON).//check 5 object
+                given().queryParam("postId", 1).contentType(ContentType.JSON).log().uri().//login uri
                     when().get(baseURI + PLACEHOLDER_GET4).
                     then().
-                    body("[4].id", lessThanOrEqualTo(5),
+                    body("[4].id", lessThanOrEqualTo(5),                         //validate fields
                                 "[4].name", containsString("vero"),
                                 "[4].email",containsStringIgnoringCase("hayden@althea.biz"),
-                                "[4].body",notNullValue());
+                                "[4].body",notNullValue()).log().body();
         }
 
     }
